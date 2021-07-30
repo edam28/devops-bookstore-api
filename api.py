d2b9fe49aae3b15401f8cfc7286d201708dfa062
Adam Clarence <adam.clarence@sky.com>
2021-07-30 14:00:48 +0100

    added prometheus

--- api.py
+++ api.py
@@ -1,37 +1,42 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 from flask_cors import CORS
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 # Initialize Flask
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+metrics = RESTfulPrometheusMetrics(app, api)
+
+metrics.info('app_info', 'Application info', version='1.4',
+             app_name='devops-bookstore-api')
 
 # A List of Dicts to store all of the books
 books = [{
-        "bookTitle": "Learning Docker" ,
-        "bookImage": "https://itbook.store/img/books/9781784397937.png",
-        "bookDescription": "Docker is a next-generation platform for simplifying application containerization life-cycle. Docker allows you to create a robust and resilient environment in which you can generate portable, composable, scalable, and stable application containers.",
-        "bookAuthors" : "Pethuru Raj, Jeeva S. Chelladhurai, Vinod Singh"
-    },
+    "bookTitle": "Learning Docker",
+    "bookImage": "https://itbook.store/img/books/9781784397937.png",
+    "bookDescription": "Docker is a next-generation platform for simplifying application containerization life-cycle. Docker allows you to create a robust and resilient environment in which you can generate portable, composable, scalable, and stable application containers.",
+    "bookAuthors": "Pethuru Raj, Jeeva S. Chelladhurai, Vinod Singh"
+},
     {
-        "bookTitle": "Kubernetes Best Practices" ,
+        "bookTitle": "Kubernetes Best Practices",
         "bookImage": "https://itbook.store/img/books/9781492056478.png",
         "bookDescription": "In this practical guide, four Kubernetes professionals with deep experience in distributed systems, enterprise application development, and open source will guide you through the process of building applications with container orchestration.",
-        "bookAuthors" : "Brendan Burns, Eddie Villalba"
-    },
+        "bookAuthors": "Brendan Burns, Eddie Villalba"
+},
     {
-        "bookTitle": "Site Reliability Engineering" ,
+        "bookTitle": "Site Reliability Engineering",
         "bookImage": "https://itbook.store/img/books/9781491929124.png",
         "bookDescription": "The overwhelming majority of a software system's lifespan is spent in use, not in design or implementation. So, why does conventional wisdom insist that software engineers focus primarily on the design and development of large-scale computing systems?",
-        "bookAuthors" : "Betsy Beyer, Chris Jones, Jennifer Petoff"
-    },
+        "bookAuthors": "Betsy Beyer, Chris Jones, Jennifer Petoff"
+},
     {
-        "bookTitle": "Mastering JavaScript" ,
+        "bookTitle": "Mastering JavaScript",
         "bookImage": "https://itbook.store/img/books/9781785281341.png",
         "bookDescription": "JavaScript is a high-level, dynamic, untyped, lightweight, and interpreted programming language. Along with HTML and CSS, it is one of the three essential technologies of World Wide Web content production, and is an open source and cross-platform technology.",
-        "bookAuthors" : "Ved Antani"
-    }, 
+        "bookAuthors": "Ved Antani"
+},
 ]
 
 # Schema For the Book Request JSON
@@ -47,8 +52,11 @@ class BookList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
 
+    @metrics.summary('requests_by_status', 'Request latencies by status', labels={'status': lambda r: r.status_code})
     def get(self):
-        return{"books": [marshal(book, bookFields) for book in books]}
+        return {
+            "books": [marshal(book, bookFields) for book in books]
+        }, 200
 
 
 api.add_resource(BookList, "/books")
